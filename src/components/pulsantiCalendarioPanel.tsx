605f5c1b4b3d082318e1f592e2c8921396f0504b
amedeo-pasanisi <amedeopasanisi@gmail.com>
2024-06-11 13:20:14 +0200

    change color on click

--- src/components/pulsantiCalendarioPanel.tsx
+++ src/components/pulsantiCalendarioPanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PanelProps } from '@grafana/data'
 import { PulsantiCalendarioOptions } from 'types'
 import { css, cx } from '@emotion/css'
@@ -8,29 +8,32 @@ import './pulsantiCalendarioPanelStyle.css'
 interface PulsantiCalendarioPanelProps extends PanelProps<PulsantiCalendarioOptions> {}
 
 export const PulsantiCalendarioPanel: React.FC<PulsantiCalendarioPanelProps> = ({ options, data, width, height, fieldConfig, id }) => {
+  const [clickedButton, setClickedButton] = useState<string>('')
 
   const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     const currentDate = new Date()
     let dataInizio
-    let dataFine = currentDate.toLocaleDateString()
+    let dataFine = currentDate.toISOString()
     switch (e.currentTarget.innerText) {
       case 'Ultima Settimana':
-        dataInizio = new Date(new Date().setDate(new Date().getDate()-7)).toLocaleDateString()
+        dataInizio = new Date(new Date().setDate(new Date().getDate()-7)).toISOString()
+        setClickedButton('week')
         break
       case 'Ultimo Mese':
-        dataInizio = new Date(currentDate.setMonth(currentDate.getMonth() - 1)).toLocaleDateString()
+        dataInizio = new Date(currentDate.setMonth(currentDate.getMonth() - 1)).toISOString()
+        setClickedButton('month')
         break
       case 'Ultimo Anno':
         let currentYear = currentDate.getFullYear()
-        dataInizio = new Date(currentYear, 0, 1).toLocaleDateString()
+        dataInizio = new Date(currentYear, 0, 1).toISOString()
+        setClickedButton('year')
         break
       case 'Tutto il periodo':
         dataInizio = options.periodoInizio
-        dataFine = options.periodoFine 
+        dataFine = options.periodoFine
+        setClickedButton('period')
         break
     }
-    console.log('inizio', `"${dataInizio ?? ''}"`)
-    console.log('fine', `"${dataFine ?? ''}"`)
     locationService.partial({
       [`var-${options.variable_data_inizio}`]: `"${dataInizio ?? ''}"`,
       [`var-${options.variable_data_fine}`]: `"${dataFine ?? ''}"`
@@ -53,16 +56,16 @@ export const PulsantiCalendarioPanel: React.FC<PulsantiCalendarioPanelProps> = (
         flex-wrap: wrap;
       `
     )}>
-      <button className='calendarioButton' onClick={e => handleButtonClick(e)}>
+      <button className={`calendarioButton ${clickedButton === 'week' && 'clicked'}`} onClick={e => handleButtonClick(e)}>
         Ultima Settimana
       </button>
-      <button className='calendarioButton' onClick={e => handleButtonClick(e)}>
+      <button className={`calendarioButton ${clickedButton === 'month' && 'clicked'}`} onClick={e => handleButtonClick(e)}>
         Ultimo Mese
       </button>
-      <button className='calendarioButton' onClick={e => handleButtonClick(e)}>
+      <button className={`calendarioButton ${clickedButton === 'year' && 'clicked'}`} onClick={e => handleButtonClick(e)}>
         Ultimo Anno
       </button>
-      <button className='calendarioButton' onClick={e => handleButtonClick(e)}>
+      <button className={`calendarioButton ${clickedButton === 'period' && 'clicked'}`} onClick={e => handleButtonClick(e)}>
         Tutto il periodo
       </button>
     </div>
